b0782b45e17b66b11cd9c5f073d661606e024a93
Lyashh <broflovski363@gmail.com>
2020-04-04 16:14:36 +0300

    index migr

--- src/database/migrations/index.ts
+++ src/database/migrations/index.ts
@@ -2,6 +2,12 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<any> {
 	return knex.schema
+<<<<<<< HEAD
+		.createTable('users', (table) => {
+			table.increments();
+			table.integer('registration_id').notNullable();
+			table.integer('role_id').notNullable().defaultTo(1);
+=======
 		.createTable('registration', (table) => {
 			table.increments('id').primary();
 			table.string('title').notNullable();
@@ -16,12 +22,24 @@ export async function up(knex: Knex): Promise<any> {
 				.references('id').inTable('registration').onDelete('CASCADE').index();
 			table.bigInteger('role_id').unsigned().notNullable()
 				.references('id').inTable('roles').onDelete('CASCADE').index();
+>>>>>>> a7ad2dc746cff409ab2f4e9ee25fc786d0a825ab
 			table.string('email').unique().notNullable();
 			table.string('password');
 			table.string('name');
 			table.timestamp('created_at').defaultTo(knex.fn.now());
 			table.timestamp('updated_at').defaultTo(knex.fn.now());
 		})
+<<<<<<< HEAD
+		.createTable('registration', (table) => {
+			table.increments();
+			table.string('title').notNullable();
+		})
+		.createTable('roles', (table) => {
+			table.increments();
+			table.string('title').notNullable();
+		})
+=======
+>>>>>>> a7ad2dc746cff409ab2f4e9ee25fc786d0a825ab
 		.createTable('products', (table) => {
 			table.increments();
 			table.string('title').notNullable();
@@ -56,7 +74,11 @@ export async function down(knex: Knex): Promise<any> {
 		.dropTable('registration')
 		.dropTable('roles')
 		.dropTable('products')
+<<<<<<< HEAD
+		.dropTable('products')
+=======
 		.dropTable('cart')
+>>>>>>> a7ad2dc746cff409ab2f4e9ee25fc786d0a825ab
 		.dropTable('delivery')
 		.dropTable('category');
 
